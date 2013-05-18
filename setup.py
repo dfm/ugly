9907cc897199d51c197d789c95dca57b2df998ac
Dan F-M <danfm@nyu.edu>
2013-05-18 15:54:36 -0400

    adding license etc.

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from setuptools import setup
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+setup(
+    name="ugly",
+    version="0.0.1",
+    description="Fucking ugly rss",
+    long_description=open("README.rst").read(),
+    author="Dan F-M",
+    author_email="danfm@nyu.edu",
+    url="https://github.com/dfm/ugly",
+    packages=["ugly"],
+    scripts=["bin/ugly"],
+    install_requires=open("requirements.txt").read().splitlines(),
+    license="MIT",
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.3",
+    ),
+)
