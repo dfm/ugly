c6d943131b18e03e1ffc7a881ec54eda8e1596ea
Dan F-M <danfm@nyu.edu>
2013-05-18 16:14:33 -0400

    readme typos

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if sys.argv[-1] == "publish":
 
 setup(
     name="ugly",
-    version="0.0.1",
+    version="0.0.2",
     description="Fucking ugly RSS",
     long_description=open("README.rst").read(),
     author="Dan F-M",
