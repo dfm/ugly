87941c71650a4c0172f8daed286f1562b5caa7c8
Dan F-M <danfm@nyu.edu>
2013-07-09 11:07:08 +0200

    invitations

--- ugly/front/__init__.py
+++ ugly/front/__init__.py
@@ -4,7 +4,10 @@
 __all__ = ["front"]
 
 import flask
-from SimpleAES import SimpleAES
+from hashlib import sha1
+
+from ..database import db
+from ..models import hash_email, Invitation
 
 front = flask.Blueprint("front", __name__, template_folder="templates")
 
@@ -16,8 +19,49 @@ def index():
 
 @front.route("/request")
 def request():
+    # Get the provided email address.
     email = flask.request.values.get("email", None)
-    if email is not None:
-        aes = SimpleAES(flask.current_app.config["AES_KEY"])
-        enc_email = aes.encrypt(email)
+    if email is None or len(email) == 0:
+        return flask.render_template("splash/request.html",
+                                     error="You need to give us an email "
+                                           "address.")
+
+    # Do a very basic check of the address.
+    spl = email.split("@")
+    if len(spl) != 2 or "." not in spl[1]:
+        return flask.render_template("splash/request.html",
+                                     error="That doesn't look like an email "
+                                           "address at all. Try again?")
+
+    # Check if an invitation already exists.
+    emailhash = hash_email(email)
+    existing = Invitation.query.filter_by(emailhash=emailhash).first()
+    if existing is not None:
+        return flask.render_template("splash/request.html",
+                                     registered=True, sent=existing.sent,
+                                     email=email)
+
+    # Save the invitation to the database.
+    invite = Invitation(email)
+    db.session.add(invite)
+    db.session.commit()
+
     return flask.render_template("splash/request.html")
+
+
+@front.route("/resend/<email>")
+def resend(email):
+    # Try to find the email.
+    emailhash = hash_email(email)
+    existing = Invitation.query.filter_by(emailhash=emailhash).first()
+    if existing is None:
+        return flask.render_template("splash/resend.html",
+                                     error="No invitation registered for that"
+                                           " email.")
+
+    # Check if the invitation is registered to send or not.
+    if not existing.sent:
+        return flask.render_template("splash/resend.html",
+                                     error="Be patient, it'll come soon.")
+
+    return flask.render_template("splash/resend.html")
--- ugly/models.py
+++ ugly/models.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__all__ = ["Invitation"]
+__all__ = ["hash_email", "Invitation"]
 
-from .db import db
-from sqlalchemy import Column, Integer, String, Boolean
+import os
+import flask
+from hashlib import sha1
+from datetime import datetime
+from SimpleAES import SimpleAES
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+
+from .database import db
+
+
+def hash_email(email):
+    return sha1(email).hexdigest()
+
+
+def encrypt_email(email):
+    aes = SimpleAES(flask.current_app.config["AES_KEY"])
+    return aes.encrypt(email)
+
+
+def decrypt_email(enc_email):
+    aes = SimpleAES(flask.current_app.config["AES_KEY"])
+    return aes.decrypt(enc_email)
 
 
 class Invitation(db.Model):
@@ -13,11 +33,52 @@ class Invitation(db.Model):
 
     id = Column(Integer, primary_key=True)
     email = Column(String)
+    emailhash = Column(String)
+    code = Column(String)
     sent = Column(Boolean)
+    created = Column(DateTime)
 
     def __init__(self, email, sent=False):
-        self.email = email
+        # Encrypt and hash the email address.
+        self.email = encrypt_email(email)
+        self.emailhash = hash_email(email)
+
+        # Generate an invitation code.
+        self.code = sha1(os.urandom(24)).hexdigest()
+
+        # Invitation sent flag.
         self.sent = sent
 
+        # Date of registration.
+        self.created = datetime.utcnow()
+
     def __repr__(self):
-        return "<Invitation(\"{0}\", sent={1})>".format(self.email, self.sent)
+        return "<Invitation(\"{0}\", sent={1})>".format(self.get_email(),
+                                                        self.sent)
+
+    def get_email(self):
+        return decrypt_email(self.email)
+
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String)
+    emailhash = Column(String)
+    joined = Column(DateTime)
+
+    def __init__(self, email):
+        # Encrypt and hash the email address.
+        self.email = encrypt_email(email)
+        self.emailhash = hash_email(email)
+
+        # Date of registration.
+        self.joined = datetime.utcnow()
+
+    def __repr__(self):
+        return "<User(\"{0}\")>".format(self.get_email())
+
+    def get_email(self):
+        return decrypt_email(self.email)
