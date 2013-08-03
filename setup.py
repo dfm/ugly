2aa661ee0dff2b562ed2d310fd2a10666d47dad2
Dan F-M <danfm@nyu.edu>
2013-08-03 21:57:04 +0000

    favicon

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup
 setup(
     name="ugly",
     packages=["ugly"],
-    package_data={"ugly": ["templates/*", "static/css/*.css"]},
+    package_data={"ugly": ["templates/*", "static/css/*.css",
+                           "static/img/*"]},
     include_package_data=True,
 )
